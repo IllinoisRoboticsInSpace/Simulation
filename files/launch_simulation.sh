091139d40c9f888c0c788d89342cb26772a20bcc
Gil Shohet <shohet2@illinois.edu>
2015-05-15 18:54:27 -0500

    added a script to launch simulation (doesn't quite work)

--- files/launch_simulation.sh
+++ files/launch_simulation.sh
@@ -1,14 +1,17 @@
 roslaunch 2015sim.launch &
 sleep 5
 rosrun gazebo_ros gazebo ../worlds/Arena.world &
-sleep 5
+sleep 10
 rosrun rviz rviz peter.rviz &
-sleep 3
-rosrun map_server map_server_params.yaml &
-sleep 3
-roslaunch sim_control.launch &
 sleep 5
+rosrun map_server map_server_params.yaml &
+sleep 5
+roslaunch sim_control.launch &
+sleep 10
 
 read -p "Press any key to kill... " -n1 -s
 killall -9 roscore
+killall -9 roslaunch
+killall -9 rosmaster
+killall -9 rosout
 killall -9 gzserver
