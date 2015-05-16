67238de020568f3afa0087e1fc1175898d75e423
Gil Shohet <shohet2@illinois.edu>
2015-05-16 16:04:09 -0500

    fixed the shell script a bit

--- files/launch_simulation.sh
+++ files/launch_simulation.sh
@@ -1,13 +1,22 @@
+# start simulation
 roslaunch 2015sim.launch &
-sleep 5
+sleep 7
+
+# start Gazebo
 rosrun gazebo_ros gazebo ../worlds/Arena.world &
 sleep 10
+
+# start rviz
 rosrun rviz rviz peter.rviz &
 sleep 5
-rosrun map_server map_server_params.yaml &
+
+# start map_server
+rosrun map_server map_server map_server_params.yaml &
 sleep 5
+
+# start move_base and manual control
 roslaunch sim_control.launch &
-sleep 10
+sleep 5
 
 read -p "Press any key to kill... " -n1 -s
 killall -9 roscore
